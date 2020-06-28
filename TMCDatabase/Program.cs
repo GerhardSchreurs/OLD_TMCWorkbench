d9540ec8a9c24cc55b5f60847b393cb6a0cf12b9
GerhardSchreurs <djnonsens@hotmail.com>
2020-06-28 22:32:55 +0200

    [WIP] Prepare for entity framework, created project TMCDatabase

---
+++ TMCDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TMCDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
