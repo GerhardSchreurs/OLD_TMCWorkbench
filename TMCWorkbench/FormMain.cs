5c68ab5c05ed9a35a725fc453ef2f8b4f20391f8
gschreurs <g.schreurs@blisss.nl>
2020-12-23 21:59:52 +0100

    Workbench remembers and resores last window position

--- TMCWorkbench/FormMain.cs
+++ TMCWorkbench/FormMain.cs
@@ -9,6 +9,7 @@ using System.Drawing;
 using TMCWorkbench;
 using System.Text;
 using TMCWorkbench.Extensions;
+using TMCWorkbench.Properties;
 
 namespace TMCWorkbench
 {
@@ -19,6 +20,8 @@ namespace TMCWorkbench
         public FormMain()
         {
             InitializeComponent();
+
+            PositionForm();
 
             EventInvoker.OnBrowserControlBrowse += Handle_BrowserControl_OnBrowse;
             EventInvoker.OnListViewControlSelected += Handle_ListViewControl_OnSelected;
@@ -375,6 +378,85 @@ namespace TMCWorkbench
         private void btnSave_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            PositionFormSave();
+        }
+
+        private void PositionFormSave()
+        {
+            // only save the WindowState if Normal or Maximized
+            switch (this.WindowState)
+            {
+                case FormWindowState.Normal:
+                case FormWindowState.Maximized:
+                    Settings.Default.WindowState = this.WindowState;
+                    break;
+
+                default:
+                    Settings.Default.WindowState = FormWindowState.Normal;
+                    break;
+            }
+
+            // reset window state to normal to get the correct bounds
+            // also make the form invisible to prevent distracting the user
+            this.Visible = false;
+            this.WindowState = FormWindowState.Normal;
+
+            Settings.Default.WindowPosition = this.DesktopBounds;
+            Settings.Default.Save();
+        }
+
+        private void PositionForm()
+        {
+            //https://stackoverflow.com/questions/937298/restoring-window-size-position-with-multiple-monitors
+            // this is the default
+            this.WindowState = FormWindowState.Normal;
+            this.StartPosition = FormStartPosition.WindowsDefaultBounds;
+
+            // check if the saved bounds are nonzero and visible on any screen
+            if (Settings.Default.WindowPosition != Rectangle.Empty &&
+                IsVisibleOnAnyScreen(Settings.Default.WindowPosition))
+            {
+                // first set the bounds
+                this.StartPosition = FormStartPosition.Manual;
+                this.DesktopBounds = Settings.Default.WindowPosition;
+
+                // afterwards set the window state to the saved value (which could be Maximized)
+                this.WindowState = Settings.Default.WindowState;
+            }
+            else
+            {
+                // this resets the upper left corner of the window to windows standards
+                this.StartPosition = FormStartPosition.WindowsDefaultLocation;
+
+                // we can still apply the saved size
+                var size = Settings.Default.WindowPosition.Size;
+
+                if (size == Size.Empty)
+                {
+                    //TODO: TEST
+                    size = new Size(Width, Height);
+                }
+
+                this.Size = size;
+            }
+        }
+
+        private static bool IsVisibleOnAnyScreen(Rectangle rect)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.IntersectsWith(rect))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
--- TMCWorkbench/Properties/Settings.Designer.cs
+++ TMCWorkbench/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TMCWorkbench.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -54,6 +54,30 @@ namespace TMCWorkbench.Properties {
         public string tmcConnectionString {
             get {
                 return ((string)(this["tmcConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0, 0, 0")]
+        public global::System.Drawing.Rectangle WindowPosition {
+            get {
+                return ((global::System.Drawing.Rectangle)(this["WindowPosition"]));
+            }
+            set {
+                this["WindowPosition"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Normal")]
+        public global::System.Windows.Forms.FormWindowState WindowState {
+            get {
+                return ((global::System.Windows.Forms.FormWindowState)(this["WindowState"]));
+            }
+            set {
+                this["WindowState"] = value;
             }
         }
     }
